7b3444499d813167e8720918e1a51ac8637c8d74
tsajitha <43032335+tsajitha@users.noreply.github.com>
2018-09-15 15:40:16 +0530

    Create Coupon.java

---
+++ Coupon.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Coupon
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		double  dis,amount,markedprice,s;
+               
+	markedprice=1000;
+ 
+        dis=25;  // 25 mean 25%			
+	
+	System.out.println("markedprice= "+markedprice);
+ 
+	System.out.println("discount rate="+dis);
+         
+        s=100-dis;
+ 
+	amount= (s*markedprice)/100;
+ 
+	System.out.println("amount after discount="+amount);
+	}
+}
