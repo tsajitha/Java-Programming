0bf10ae091526c9aa378dbf20ec5d43de152d465
tsajitha <43032335+tsajitha@users.noreply.github.com>
2018-09-15 15:26:57 +0530

    Create Operator.java

---
+++ Operator.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Operator
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int a=5, b=4;
+  int c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s;
+  c=a+b;
+  d=a-b;
+  e=a*b;
+  f=b/a;
+  g=b%a;
+  h=b++;
+  i=b--;
+  j=a&b;
+  k=a|b;
+  l=a^b;
+  m=~a;
+  n=a<<2;
+  o=>>2;
+  p=a>>>2;
+  q=a&&b;
+  r=a||b;
+  s=!(a&&b);
+   System.out.println(c+''+d''+e''+f''+g''+h''+i''+j''+k''+l''+m''+n''+o''+p''+q''+r''+s);
+  	
+	}
+}
