e106697e977762a1d4e2ef89fc0ad5dd20296f1b
tsajitha <43032335+tsajitha@users.noreply.github.com>
2018-09-17 15:41:34 +0530

    Create Flop.java

---
+++ Flop.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Flop
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int d=100;
+    if(d>=50)
+    System.out.println("Box Office Hit");
+    else
+    System.out.println("Flop");
+
+  }
+}
+	}
+}
