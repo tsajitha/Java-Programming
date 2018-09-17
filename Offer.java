38c6a5eb2de853f4003fbc69b80e65ed7ef8e671
tsajitha <43032335+tsajitha@users.noreply.github.com>
2018-09-17 15:22:30 +0530

    Create Offer.java

---
+++ Offer.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Offer
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   int Sunday=0,Monday=1,Tuesday=2,Wednesday=3,Thursday=4,Friday=5,Saturday=6;
+switch(4)
+{
+case '0':
+System.out.println("Free Personal Pizza on purchase of medium Pizza");
+break;
+case '1':
+System.out.println("Rs.101 OFF on a bill of Rs.400 and above");
+break;
+case '2':
+System.out.println("Free Plain Garlic Bread on another medium pizza");
+break;
+case '3':
+System.out.println("Buy 1 Get 1 Free on any medium pizza");
+break;
+case '4':
+System.out.println("Buy any medium pizza and get 50% off on another medium pizza");
+break;
+case '5':
+System.out.println("Tastiest pizza 20% off on bill of Rs.399 or above");
+break;
+case '6':
+System.out.println("Get Together Offer Free Garlic Bread and Dip on a min bill of Rs.350");
+break;
+default:
+System.out.println("No Offer");
+}
+	}
+}
