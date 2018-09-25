55e703890d893010d5724db261f75d1edb916cc5
tsajitha <43032335+tsajitha@users.noreply.github.com>
2018-09-25 15:10:55 +0530

    Create Package.java

---
+++ Package.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Package
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("Min value of 523, 456 is: "+Math.min(523, 456));
+        System.out.println("Min value of 34.23, 45 is: "+Math.min(34.23, 45));
+        System.out.println("Min value of 41.89, 23.45 is: "+Math.min(41.89, 23.45));
+	}
+}
