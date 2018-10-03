44267b97fd6b72d2cbbcd7c5fa21abcfd2d4f7a7
tsajitha <43032335+tsajitha@users.noreply.github.com>
2018-10-03 12:40:21 +0530

    Update Vowel.java

--- Vowel.java
+++ Vowel.java
@@ -1,17 +1,18 @@
 class Vowel {
   public static void main(String[] args) {
-  char ch = a;
+  char ch = 'a' ;
   switch(ch)
   {
-  case a:
-  case e:
-  case i:
-  case o:
-  case o:
-  case u:
-  System.out.println(ch+ "ch is a vowel");
-  else
-  System.out.println(ch+ "ch is a consonant");
+  case 'a':
+  case 'e':
+  case 'i':
+  case 'o':
+  case 'o':
+  case 'u':
+  System.out.println(ch+ " is a vowel");
+      break;
+    default:
+  System.out.println(ch+ " is a consonant");
   }
   }
   }
