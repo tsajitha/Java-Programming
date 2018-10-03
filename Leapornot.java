750e7f4dffdc0e26e12163d5342c7e215f53cabf
tsajitha <43032335+tsajitha@users.noreply.github.com>
2018-10-03 12:04:43 +0530

    Update Leapornot.java

--- Leapornot.java
+++ Leapornot.java
@@ -1,7 +1,7 @@
 public class Leapornot {
    public static void main(String[] args)
     {
-       int year;
+       int year = 2016;
                
        if(year % 4 == 0)
                System.out.println("Year " + year + " is a leap year");
