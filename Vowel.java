50d6e0759ff4c164f08c5218c851399d718ab772
tsajitha <43032335+tsajitha@users.noreply.github.com>
2018-10-03 12:33:12 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,17 @@
+class Vowel {
+  public static void main(String[] args) {
+  char ch = a;
+  switch(ch)
+  {
+  case a:
+  case e:
+  case i:
+  case o:
+  case o:
+  case u:
+  System.out.println(ch+ "ch is a vowel");
+  else
+  System.out.println(ch+ "ch is a consonant");
+  }
+  }
+  }
