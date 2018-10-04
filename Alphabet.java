c43831642a300382a86e531ba976b002279c97d9
tsajitha <43032335+tsajitha@users.noreply.github.com>
2018-10-04 11:35:01 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,9 @@
+class Alphabet {
+  public static void main(String[] args) {
+    char c = 'a';
+    if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    System.out.println(c+ " is an alphabet");
+    else
+    System.out.println(c+ " is not an alphabet");
+  }
+}
