f4af80abaa0dc44f175ed81144da055c63b36f13
tsajitha <43032335+tsajitha@users.noreply.github.com>
2018-10-04 12:03:56 +0530

    Create Largerthree.java

---
+++ Largerthree.java
@@ -0,0 +1,11 @@
+class Largerthree {
+  public static void main(String[] args) {
+  int a=1,b=3,c=5;
+  if(a >= b && a>= c)
+  System.out.println(a+ " is larger);
+  if(b >= a && b >= c)
+  System.out.println(a+ " is larger);
+  else
+  System.out.println(c+ " is larger);
+  }
+  }
