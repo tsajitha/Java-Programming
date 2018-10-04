ab7c45f9935740d4438a5d6796187f638c625e4d
tsajitha <43032335+tsajitha@users.noreply.github.com>
2018-10-04 12:30:52 +0530

    Create Ninteger.java

---
+++ Ninteger.java
@@ -0,0 +1,8 @@
+class Ninteger {
+  public static void main(String[] args) {
+   int num = 500,sum = 0;
+   for(int i = 1; i <= num; ++i)
+   sum += i;
+   System.out.println("Sum is " +sum) ;
+  }
+}
